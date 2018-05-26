dec13a832ea80a3269e0bca1a89c3cc183ed7117
PhoenixCastellan <836102291@qq.com>
2018-05-26 15:17:20 +0800

    创建项目

---
+++ src/test/java/JavaToolsStudy/MavenInAction/AppTest.java
@@ -0,0 +1,20 @@
+package JavaToolsStudy.MavenInAction;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
